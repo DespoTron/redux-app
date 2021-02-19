a4d56550f00d1ceb26ac5a176dad2c348dda5e2d
Thai Do <thai.hl.do@gmail.com>
2021-02-19 15:46:34 -0800

    Basic redux app

--- src/components/NewComp.js
+++ src/components/NewComp.js
@@ -1,33 +1,32 @@
 import React, { Component } from 'react'
-// import { connect } from 'react-redux'
+import { connect } from 'react-redux'
 
 class NewComp extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      message: 'Subscribe to Learn Redux',
-    }
-  }
-
   styles = {
     fontStyles: 'italic',
     color: 'purple',
   }
 
-  ButtonChange = () => {
-    this.setState({
-      message: 'Thank you for subscribing',
-    })
-  }
   render() {
     return (
       <div className="App">
-        <h3 style={this.styles}>{this.state.message}</h3>
-        <button onClick={this.ButtonChange}>Subscribe</button>
+        <h3 style={this.styles}>{this.props.message}</h3>
+        <button onClick={this.props.ButtonChange}>Subscribe</button>
       </div>
     )
   }
 }
 
-export default NewComp
+const mapStatetoProps = (state) => {
+  return {
+    message: state.message,
+  }
+}
+
+const mapDispatchtoProps = (dispatch) => {
+  return {
+    ButtonChange: () => dispatch({ type: 'Message change' }),
+  }
+}
+
+export default connect(mapStatetoProps, mapDispatchtoProps)(NewComp)
