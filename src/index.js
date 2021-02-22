dd76a0469450fd446ff642518aeef498d7242dee
Thai Do <thai.hl.do@gmail.com>
2021-02-22 15:10:29 -0800

    Revised version using redux counter

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
-import { createStore } from 'redux'
-import reducer from './Store/Reducer'
+import store from './app/store'
+import { Provider } from 'react-redux'
+import * as serviceWorker from './serviceWorker'
 
-const store = createStore(reducer)
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 )
 
